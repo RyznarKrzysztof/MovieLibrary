03fe6bb5d53f9bd023ad3641f69d35276b383644
Krzysztof Ryznar <kryznar@sii.pl>
2018-10-16 12:00:26 +0200

    main changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,61 +19,59 @@ public class Main {
         //POJOs (Plain Old Java Objects), or to and from a general-purpose
         //JSON Tree Model (JsonNode), as well as related functionality for performing conversions.
 
-//        ClassLoader classLoader = new Main().getClass().getClassLoader();
-//        File file = new File(classLoader.getResource("MoviesJSON").getFile());
+        ClassLoader classLoader = new Main().getClass().getClassLoader();
+        File file = new File(classLoader.getResource("MoviesJSON").getFile());
 
-        ArrayList<Movie> movies = objectMapper.readValue(new File("C:\\Users\\kryznar\\Desktop\\Movie.json"),
-                                                         new TypeReference<List<Movie>>(){});
+        ArrayList<Movie> movies = objectMapper.readValue(file, new TypeReference<List<Movie>>(){});
         //readValue - Method to deserialize JSON content from given file into given Java type.
 
 
-
-
-
-//        for(Movie m : movies){
-//            System.out.println("Title = " + m.getTitle());
-//            System.out.println("Director = " + m.getDirector());
-//            System.out.println("Year = " + m.getYear());
-//            System.out.println("Genre = " + m.getGenre());
-//            System.out.print("Actors: ");
-//            for(int i = 0; i<m.getActors().length; i++){
-//                if(i == m.getActors().length - 1){
-//                    System.out.println(m.getActors()[i]);
-//                }else {
-//                    System.out.print(m.getActors()[i] + ", ");
-//                }
-//            }
-//            System.out.println();
-//        }
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Search by actor - press 1");
-//        System.out.println("Search by genre and date range - press 2");
-//        int n = scanner.nextInt();
-//        scanner.nextLine();
-//        if(n == 1){
-//            System.out.println("Please provide Actor's name");
-//            String name = scanner.nextLine();
-//            for(Movie m : movies){
-//                for(int i = 0; i < m.getActors().length; i++){
-//                    if(m.getActors()[i].equals(name)){
-//                        System.out.println(m.getTitle());
-//                    }
-//                }
-//            }
-//        }else if(n == 2){
-//            System.out.println("Please provide genre");
-//            String genre = scanner.nextLine();
-//            System.out.println("Please provide min");
-//            int min = scanner.nextInt();
-//            System.out.println("Please provide max");
-//            int max = scanner.nextInt();
-//            for(Movie m : movies){
-//                if(m.getGenre().equals(genre) && m.getYear() >= min && m.getYear() <= max){
-//                    System.out.println(m.getTitle());
-//                }
-//            }
-//        }else{
-//            System.out.print("Incorrect number");
-//        }
+        for(Movie m : movies){
+            System.out.println("Title = " + m.getTitle());
+            System.out.println("Director = " + m.getDirector());
+            System.out.println("Year = " + m.getYear());
+            System.out.println("Genre = " + m.getGenre());
+            System.out.print("Actors: ");
+            for(int i = 0; i<m.getActors().length; i++){
+                if(i == m.getActors().length - 1){
+                    System.out.println(m.getActors()[i]);
+                }else {
+                    System.out.print(m.getActors()[i] + ", ");
+                }
+            }
+            System.out.println();
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Search by actor - press 1");
+        System.out.println("Search by genre and date range - press 2");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        if(n == 1){
+            System.out.println("Please provide Actor's first name");
+            String name = scanner.nextLine();
+            System.out.println("Please provide Actor's second name");
+            String surname = scanner.nextLine();
+            for(Movie m : movies){
+                for(int i = 0; i < m.getActors().length; i++){
+                    if(m.getActors()[i].getName().equals(name) && m.getActors()[i].getSurname().equals(surname)){
+                        System.out.println(m.getTitle());
+                    }
+                }
+            }
+        }else if(n == 2){
+            System.out.println("Please provide genre");
+            String genre = scanner.nextLine();
+            System.out.println("Please provide min");
+            int min = scanner.nextInt();
+            System.out.println("Please provide max");
+            int max = scanner.nextInt();
+            for(Movie m : movies){
+                if(m.getGenre().equals(genre) && m.getYear() >= min && m.getYear() <= max){
+                    System.out.println(m.getTitle());
+                }
+            }
+        }else{
+            System.out.print("Incorrect number");
+        }
     }
 }
